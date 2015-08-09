63e46c8f11eca691fb045ff752dca4af568b22dd
Dean Brettle <dean@brettle.com>
2015-08-09 11:39:18 -0700

    Add version constraint.

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('brettle:accounts-multiple', 'server');
+  api.use('brettle:accounts-multiple@0.0.1', 'server');
   api.use('underscore', 'server');
   api.addFiles('accounts-logout-to-switch.js', 'server');
 });
