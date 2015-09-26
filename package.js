e972648e564ea01c25d4d120ffbca3cc558a157a
Dean Brettle <dean@brettle.com>
2015-09-26 14:51:58 -0700

    Publish 0.4.2 which references versions that work with Meteor 1.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'brettle:accounts-logout-to-switch',
-  version: '0.4.1',
+  version: '0.4.2',
   summary: 'Requires that signed up users sign out before they sign in to ' +
     'another existing account.',
   git: 'https://github.com/brettle/meteor-accounts-logout-to-switch.git',
@@ -11,8 +11,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.4');
-  api.use('brettle:accounts-multiple@0.0.1', 'server');
-  api.use('brettle:accounts-login-state@0.0.2', 'server');
+  api.use('brettle:accounts-multiple@0.3.1', 'server');
+  api.use('brettle:accounts-login-state@0.0.3', 'server');
   api.use('underscore', 'server');
   api.export('AccountsLogoutToSwitch');
   api.addFiles('accounts-logout-to-switch.js', 'server');
@@ -25,7 +25,7 @@ Package.onTest(function(api) {
   api.use('brettle:accounts-logout-to-switch');
   api.use('brettle:accounts-testing-support');
   api.use('brettle:accounts-anonymous');
-  api.use('brettle:accounts-multiple@0.1.0');
+  api.use('brettle:accounts-multiple@0.3.1');
   api.use('service-configuration', 'server');
   api.use('accounts-base', 'server');
   api.addFiles('accounts-logout-to-switch-tests.js', 'server');
